528d91c39047717b48930f69509a12a9def1e7ea
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-10-21 13:11:38 -0700

    update

--- assignment1.py
+++ assignment1.py
@@ -9,16 +9,21 @@ def clicker():
     
     while target==(140,450):
         target = pyautogui.position()
-        pyautogui.doubleClick()
+        pyautogui.click()
         if pyautogui.pixelMatchesColor(670, 630, (255, 255, 255)):
             pyautogui.click(670, 630)
         elif pyautogui.pixelMatchesColor(700, 680, (175, 176, 170)):
             pyautogui.click(700,680)
         elif pyautogui.pixelMatchesColor(700, 750, (178, 178, 166)):
             pyautogui.click(700, 750)
-        if pyautogui.pixelMatchesColor(700, 820, (174, 172, 159)):
+        elif pyautogui.pixelMatchesColor(700, 820, (174, 172, 159)):
             pyautogui.click(700, 820)
+        elif pyautogui.pixelMatchesColor(693, 495, (15, 40, 54)):
+            pyautogui.click(693, 495)
+        elif pyautogui.pixelMatchesColor(700, 890, (189, 187, 174)):
+            pyautogui.click(700, 890)
+        elif pyautogui.pixelMatchesColor(700, 960, (175, 176, 162)):
+            pyautogui.click(700, 960)
         pyautogui.click(140, 450)
-        
 clicker()
 
